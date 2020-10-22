2abd9a70d0557eb72cc7d1b85aed5ccd1676fd36
zackad <zackad.dev@gmail.com>
2020-10-22 14:18:03 +0700

    feat: add spacing and fix code style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Textarea } from './Textarea'
 import { Table } from './Table'
+import style from './App.module.css'
 
 function App() {
   const [motorCycle, setMotorCycle] = React.useState(0)
@@ -8,19 +9,21 @@ function App() {
   const [heavyVehicle, setHeavyVehicle] = React.useState(0)
   const [total, setTotal] = React.useState(0)
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     const data = event.target.value
 
-    setMotorCycle([...data].filter(l => parseInt(l) === 1).length)
-    setLightVehicle([...data].filter(l => parseInt(l) === 2).length)
-    setHeavyVehicle([...data].filter(l => parseInt(l) === 3).length)
+    setMotorCycle([...data].filter((l) => parseInt(l) === 1).length)
+    setLightVehicle([...data].filter((l) => parseInt(l) === 2).length)
+    setHeavyVehicle([...data].filter((l) => parseInt(l) === 3).length)
     setTotal(data.replace(/\D+/g, '').length)
   }
 
   return (
-    <div>
-      <Textarea onChange={handleInputChange} />
-      <Table motorCycle={motorCycle} lightVehicle={lightVehicle} heavyVehicle={heavyVehicle} total={total} />
+    <div className={style.App}>
+      <div className={style.container}>
+        <Textarea onChange={handleInputChange} />
+        <Table motorCycle={motorCycle} lightVehicle={lightVehicle} heavyVehicle={heavyVehicle} total={total} />
+      </div>
     </div>
   )
 }
